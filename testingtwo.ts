5b791d535f630e55d50046beebea08f661e9f02e
Manjunath <manjunathshetty88@gmail.com>
2026-08-04 23:02:09 +0530

    my second commit

--- testingtwo.ts
+++ testingtwo.ts
@@ -1,2 +1,3 @@
-This is for testing second file name 1
-This is for testing second file name 2
+//This is for testing second file name 1
+//This is for testing second file name 2
+//This modfied file now.
